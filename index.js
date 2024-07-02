e576721bbd955d9898fe822d16c1cd811274c56c
Lokendra Singh Rao <lokendras.715@gmail.com>
2024-07-02 23:36:08 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 8080
 const app = express()
 app.use(cors())
 app.use("/images", express.static("images"))
-
+app.get("/", (req,res) => {
+    res.send("Welcome to ecommerce api by Lokenrao")
+})
 app.get("/products", (req, res) => {
     res.send(products)
 })
